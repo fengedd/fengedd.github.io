c2c60bd226d1ac64b8ad938be4ec7ec5b13ca889
unknown <fengedd@gmail.com>
2019-02-13 09:00:43 -0800

    Fixed pipe

--- src/app/pipes/github-projects.pipe.ts
+++ src/app/pipes/github-projects.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class GithubProjectsPipe implements PipeTransform {
 
-  transform(json: any): any {
+  transform(json: any): Object {
     const name = json.name;
     const html_url = json.html_url;
     const description = json.description;
@@ -14,20 +14,34 @@ export class GithubProjectsPipe implements PipeTransform {
     const git_url = json.git_url;
     const language = json.language;
     // TODO: Add date pipe short for created_at and updated_at
+    
     const result =
     `
-    <ul>
-    <li><b>${name}</b> </li>
-    <li><b>${html_url}</b> </li>
-    <li><b>${created_at}</b> </li>
-    <li><b>${updated_at}</b> </li>
-    <li><b>${git_url}</b> </li>
-    <li><b>${language}</b> </li>
-    </ul>
+    <div class="media">
+  <img src="..." class="mr-3" alt="...">
+  <div class="media-body">
+    <h5 class="mt-0" href=><a href="${html_url}">${name}</a> ${updated_at}</h5>    
+    ${description}
+    ${git_url}
+    ${language}
+  </div>
+  </div>
+    
+    
     `;
+   /* 
+   const result = {
+     name,
+     html_url,
+     description,
+     created_at,
+     updated_at,
+     git_url,
+     language
+   };
+   */
 
     return result;
-
   }
 }
 
